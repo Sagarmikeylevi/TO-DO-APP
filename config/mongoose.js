2c8b6178604da2d32567b6ce5356489087331378
unknown <sagarmikeylevi@gmail.com>
2023-02-20 15:43:58 +0530

    changes in .env file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/TODO_APP_DB');
+const mongoose = require('mongoose');
+require('dotenv').config();
+/* mongoose.set('strictQuery', false); is a method that is used to configure Mongoose to be less strict when querying MongoDB collections.*/
+mongoose.set('strictQuery', false);
+/* The "useNewUrlParser" option is used to configure the MongoDB driver to use the new url parser instead of the deprecated one. */
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
-db.on('error' , console.error.bind(console, 'error connecting to db'));
+/* console.error.bind() is a method that allows you to create a new function that has the same behavior as the console.error() function */
+db.on('error' , console.error.bind(console , "Error connecting to MongoDB"));
 
-db.once('open' , function(){
-    console.log('Successfully connected to the database');
+db.on('connected', () => {
+    console.log('Mongoose default connection open');
 });
+
+module.exports = db;
